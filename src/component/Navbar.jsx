a3c4792ee89e5159a14aef2295b801f0658de40b
gojung12 <148044646+gojung12@users.noreply.github.com>
2024-09-23 13:53:04 +0700

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className="flex items-center justify-between mb-3">
         <div className="flex ml-10 mt-5 font-['cinzel'] font-semibold">
           <a href="/" className="">Fashion & Accessories</a>
-          <a href="https://pts-emir.vercel.app/Fragrance" className="ml-5" >Fragrance & Beauty</a>
+          <a href="/Fragrance" className="ml-5" >Fragrance & Beauty</a>
         </div>
         <div className="flex items-center justify-center">
           <img src="LogoDior.png" alt="Logo Dior" className="h-6 mt-4 mr-[360px]" />
